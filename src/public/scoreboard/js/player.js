16c2ccd65506cdf4ec0eb518c4323a70f12a9fe9
Tri Nguyen <mail@trimn.net>
2020-02-27 02:49:11 +0100

    Add Scoreboard view components

---
+++ src/public/scoreboard/js/player.js
@@ -0,0 +1,12 @@
+/**
+ * Player component
+ */
+function Player(player, index) {
+
+  return e('div', { key: `player-${index}`, className: 'player' },
+    e('div', { className: 'player-rank' }, (index + 1)),
+    e('div', { className: 'player-name' }, player.name),
+    e('div', { className: 'player-titles' }, player.title),
+    e('div', { className: 'player-score' }, player.score)
+  )
+}
---
+++ src/public/scoreboard/js/scoreboard.js
@@ -0,0 +1,62 @@
+const e =  React.createElement
+
+
+/**
+ * Main Scoreboard component
+ */
+function Scoreboard() {
+  const [players, setPlayers] = React.useState([])
+
+  React.useEffect(() => {
+
+    // Setup WS connection
+    const ws = new WebSocket('ws://localhost:3001/scoreboard/ws')
+    ws.onopen = handleConnected
+    ws.onmessage = receiveMessage
+
+    // TODO: Test data / to be removed
+    setPlayers([
+      { name: 'Foo', title: '', score: 10 },
+      { name: 'Bar', title: '', score: 10  },
+      { name: 'Baz', title: '', score: 10  }
+    ])
+  }, [])
+
+  /**
+   * Message receive handler
+   * @param message The envelope from the server containing `type` and `data`
+   */
+  function receiveMessage(message) {
+    const { type, data } = JSON.parse(message.data)
+
+    switch (type) {
+      case 'player_connected':
+        setPlayers(players => [...players, data])
+        break
+
+      case 'player_score':
+        //...
+        break
+
+      default:
+        console.warn('Unknown event')
+    }
+  }
+
+  function handleConnected() {
+    console.info('Im connected!')
+  }
+
+  return e(React.Fragment, null,
+    e('div', { className: 'scoreboard-title' },
+      e('div', { className: 'player-rank' }, 'Rank'),
+      e('div', { className: 'player-name' }, 'Name'),
+      e('div', { className: 'player-titles' }, 'Title'),
+      e('div', { className: 'player-score' }, 'Score')
+    ),
+    players.map((player, index) => Player(player, index))
+  )
+}
+
+
+
