acfe4f05ea9fb68f2c24bf3fc6d78c1cdbcb4dd5
Tri Nguyen <mail@trimn.net>
2020-03-02 22:19:35 +0100

    Join titles by comma

--- src/public/scoreboard/js/player.js
+++ src/public/scoreboard/js/player.js
@@ -6,7 +6,7 @@ function Player(player, index) {
   return e('div', { key: `player-${index}`, className: 'player' },
     e('div', { className: 'player-rank' }, (index + 1)),
     e('div', { className: 'player-name' }, player.name),
-    e('div', { className: 'player-titles' }, player.title),
+    e('div', { className: 'player-titles' }, player.titles.join(', ')),
     e('div', { className: 'player-score' }, player.score)
   )
 }
